5ae780d204c7a36df43c23b5f241dc2963c318de
pagiannis <pagiannis04@gmail.com>
2024-09-20 21:05:00 +0300

    Initial Commit

---
+++ BuyMe/BuyMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyMeApp.swift
+//  BuyMe
+//
+//  Created by Γιάννης on 20/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BuyMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuyMeTests/BuyMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuyMeTests.swift
+//  BuyMeTests
+//
+//  Created by Γιάννης on 20/9/24.
+//
+
+import Testing
+@testable import BuyMe
+
+struct BuyMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
